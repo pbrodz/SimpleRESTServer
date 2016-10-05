89c37bd5efe3b2d8d07ec8ed939acbb3a6173e71
pbrodz <pbrodz@PBRODZSAW>
2016-10-05 11:14:33 -0700

    Add project files.

---
+++ SimpleRESTServer/Controllers/PersonController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleRESTServer.Controllers
+{
+    public class PersonController : ApiController
+    {
+        // GET: api/Person
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Person1", "Person2" };
+        }
+
+        // GET: api/Person/5
+        public string Get(int id)
+        {
+            return "some Person";
+        }
+
+        // POST: api/Person
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Person/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Person/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
